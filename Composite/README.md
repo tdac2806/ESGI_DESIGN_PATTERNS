5fd0c7b30f63ea5b3be6d699a93a1874990da55f
tdac2806 <tristandacosta@gmail.com>
2022-07-06 16:42:42 +0200

    Composite

---
+++ Composite/README.md
@@ -0,0 +1,10 @@
+﻿# Composite
+Ce pattern offre un cadre de conception d'objet dont on ne connait pas la profondeur.  
+On peut utiliser un arbre en tant qu'analogie.  
+Les "Clients" interagissent avec les objets sans connaitre la structure de l'arbre.
+
+
+### Exemple Général
+![](Img/img2.png)
+### Exemple Concret
+![](Img/img1.png)
---
+++ Composite/Utilisateur.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Utilisateur
+{
+  static void Main(string[] args)
+  {
+    Societe societe1 = new SocieteSansFiliale();
+    societe1.ajouteVehicule();
+    Societe societe2 = new SocieteSansFiliale();
+    societe2.ajouteVehicule();
+    societe2.ajouteVehicule();
+    Societe groupe = new SocieteMere();
+    groupe.ajouteFiliale(societe1);
+    groupe.ajouteFiliale(societe2);
+    groupe.ajouteVehicule();
+    Console.WriteLine(" Coût d'entretien total du groupe : " + groupe.calculeCoutEntretien());
+  }
+}
